237b5d93b16fe4d7b6bfe639bdb5d207150c52f7
jean-marie <jabengzoa@gmail.com>
2024-05-04 19:35:15 +0200

    ajout_run_pipeline

--- etl/extract_data.py
+++ etl/extract_data.py
@@ -15,9 +15,10 @@ def extract(filepath: object, select_col: list) -> object :
         params : filepath de type str qui est le lien du site ou les données se trouvent
         output : Dataframe pandas extrait sous format CSV
     """
+    logger.info('Stard extraction process for real estate data.')
     try:
         # Lecture du fichier text
-        logger.info('Stard extraction process.')
+        
         df_val = pd.read_csv(filepath, delimiter = "|", low_memory = False)
         df_val = df_val[select_col]
         logger.info(f'{filepath} : extracted correctly {filepath} record from csv file')
@@ -27,3 +28,20 @@ def extract(filepath: object, select_col: list) -> object :
         df_val = pd.DataFrame()
         logger.info('The process of extraction completed.')
     return df_val 
+
+
+def extract_dpt(filepath : object):
+    """
+        This fonction extracte data of France department
+        :param filepath : str file in csv data
+        :output : data extracted from csv file 
+    """
+    logger.info('Stard extraction process for departement data.')
+    try:
+        dept_df = pd.read_csv(filepath)
+        logger.info(f'{filepath} : extracted correctly {filepath} record from csv file')
+    except Exception as e:
+        logger.exception(f'{filepath} : exception {e} encountered while extracting the csv_file_name file')
+        dept_df = pd.read_csv()
+    logger.info('The process of extraction completed.')
+    return dept_df
